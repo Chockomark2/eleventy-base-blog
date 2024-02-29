12c4474d4abfaf109c9cd5e1847ed9b80dde7720
chockomark2 <oloughlinmarketing58@gmail.com>
2024-02-29 14:46:31 +0000

    took out the plug ins and changed my info in metadata,js

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
+	title: "Eleventy Base Blog",
 	url: "https://example.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am writing about my experiences as with Eleventy.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
+		name: "Mick O Loughlin",
+		email: "oloughlinmarketing58@gmail.com",
 		url: "https://example.com/about-me/"
 	}
 }
