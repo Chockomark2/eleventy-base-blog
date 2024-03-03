8c56c646a51dbb22073e635e1d2ea04fb922db09
Chockomark2 <160872755+Chockomark2@users.noreply.github.com>
2024-03-03 17:28:25 +0000

    Update blog.11tydata.js
    changed content

--- content/blog/blog.11tydata.js
+++ content/blog/blog.11tydata.js
@@ -1,6 +1,26 @@
+const { DateTime } = require("luxon");
+
 module.exports = {
-	tags: [
-		"posts"
-	],
-	"layout": "layouts/post.njk",
+  eleventyComputed: {
+    permalink: (data) => {
+      return `/blog/${data.page.fileSlug}/`;
+    },
+  },
+
+  // Function to filter out drafts based on the front matter
+  collectionItemTemplateContent: function(content) {
+    const matter = require("gray-matter");
+    const { data } = matter(content);
+    return data.draft !== true;
+  },
+
+  // Function to sort posts by date
+  sortByDate: function(a, b) {
+    return DateTime.fromJSDate(a.date) - DateTime.fromJSDate(b.date);
+  },
+
+  // Function to reverse the order of posts
+  reverseOrder: function(array) {
+    return array.reverse();
+  },
 };
